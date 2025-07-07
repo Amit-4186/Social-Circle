39da078f76b126a3c385071f2671b3328aa0e510
Amit-4186 <amitkrpatel4.186@gmail.com>
2025-07-07 13:49:50 +0530

    Fixed - Login without verification Added ProfileExists check

--- app/src/main/java/com/example/socialcircle/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/socialcircle/screens/LoadingScreen.kt
@@ -1,22 +1,15 @@
 package com.example.socialcircle.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun LoadingScreen(isLoaded: Boolean, loadingComplete: () -> Unit) {
-    if(isLoaded){
-        loadingComplete
-    }
-    else {
-        Column {
-            CircularProgressIndicator()
-            Button(onClick = loadingComplete) {
-                Text("Load")
-            }
-        }
+fun LoadingScreen() {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator()
     }
 }
--- app/src/main/java/com/example/socialcircle/screens/ProfileCreationScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.socialcircle.screens
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun ProfileCreationScreen(navController: NavController){
-
-}
